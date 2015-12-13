0870795c2095986dcf5b1790c14636f043b54b5e
rafael <pantera@localhost.localdomain>
2015-12-13 20:42:12 -0200

    Improved

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ require('http').createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('Server recieved a request\n');
   res.write('Client IP: ' + (req.headers['x-forwarded-for'] || req.connection.remoteAddress) + '\n');
-  res.write('Server IP: ' + serverIps.map(function(v) { return v.address; }));
-  res.end('\n');
+  res.end('Server IP: ' + serverIps.map(function(v) { return v.address; }) + '\n');
 }).listen(3000, '0.0.0.0', function() {
   console.log('server is up');
 });
