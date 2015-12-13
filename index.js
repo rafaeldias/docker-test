72adfacd36624decfc2dbe45d9b070616fba560f
rafael <pantera@localhost.localdomain>
2015-12-13 18:12:01 -0200

    Show server and client IP on each request

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+var serverIps = (function(ifaces) {
+      var ips; 
+      for (var iname in ifaces) {
+        if ((ips = (ifaces[iname].filter(function (v) {
+          return 'IPv4' === v.family && v.internal !== true
+        }))).length) {
+          break;
+        };
+      }
+      return ips || [];
+    })(require('os').networkInterfaces());
+
 require('http').createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Server recieved a request');
+  res.write('Server recieved a request\n');
+  res.write('Client IP: ' + (req.headers['x-forwarded-for'] || req.connection.remoteAddress) + '\n');
+  res.write('Server IP: ' + serverIps.map(function(v) { return v.address; }));
+  res.end('\n');
 }).listen(3000, '0.0.0.0', function() {
   console.log('server is up');
 });
